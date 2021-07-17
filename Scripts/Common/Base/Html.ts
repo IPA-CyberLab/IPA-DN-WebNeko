04ce7556dfa821c068224b087865a406d0ea0658
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-07-17 14:01:42 +0900

    途中さん

--- Scripts/Common/Base/Html.ts
+++ Scripts/Common/Base/Html.ts
@@ -37,6 +37,22 @@ import { Util } from "./Util";
 
 export class Html
 {
+    // webp をサポートさているかどうか確認
+    public static async IsWebpSupportedAsync(): Promise<boolean>
+    {
+        // If the browser doesn't has the method createImageBitmap, you can't display webp format
+        if (!self.createImageBitmap) return false;
+
+        // Base64 representation of a white point image
+        const webpData = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
+
+        // Retrieve the Image in Blob Format
+        const blob = await fetch(webpData).then(r => r.blob());
+
+        // If the createImageBitmap method succeeds, return true, otherwise false
+        return createImageBitmap(blob).then(() => true, () => false);
+    }
+
     // Web ブラウザの種類を取得
     public static GetBrowserType(): string
     {
